c82ab352b06ff0fe060c2e7269fd142e32e547f9
Isil Donmez <isil.donmez@gmail.com>
2018-02-16 13:07:20 +0000

    Add save and database methods into Idea class

--- idea.rb
+++ idea.rb
@@ -1,6 +1,13 @@
 class Idea
 
   def save
+    database.transaction do |db|
+      db['ideas'] ||= []
+      db['ideas'] << {title: 'diet', description: 'pizza all the time'}
+    end
   end
 
+  def database
+    @database ||= YAML::Store.new "ideabox"
+  end
 end
